57fd511a48c2733d3e6d938b21748558289e83a1
Carolaine Vieira <54178722+carolaine-vieira@users.noreply.github.com>
2021-11-02 13:51:12 -0300

    Adicionando filtro

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,9 +1,63 @@
 export default function Modal(element, index) {
-  const folders = document.querySelectorAll(".screen .open-folder");
-  var teste = $(element).attr("class");
-  console.log(teste);
-
   const current = $(element).attr("class");
+
+  const projects = [
+    {
+      id: 4,
+      image: "../../assets/Songster.png",
+      title: "Projeto para estudar React",
+      description: "Criei a página inicial. Falta o resto...",
+      type: "React",
+    },
+    {
+      id: 4,
+      image: "../../assets/Portfolio-2.png",
+      title: "Meu suposto novo portfólio",
+      description:
+        "Estava achando o meu antigo bem simples. Fiz esse, porém, conforme assistia as palestras da faculdade pensei em fazer algo mais interativo então criei este aqui.",
+      type: "Figma",
+    },
+    {
+      id: 4,
+      image: "../../assets/Alternativo.png",
+      title: "Alternativo",
+      description:
+        "Este tema foi criado para ser vendido no Codester. Não foi usado nada além de HTML, CSS e JS. Tumblr é uma plataforma simples de blogs pessoais.",
+      type: "HTML",
+    },
+    {
+      id: 4,
+      image: "../../assets/Novamente.png",
+      title: "Novamente",
+      description:
+        "Este tema foi criado para ser vendido no Codester. Não foi usado nada além de HTML, CSS e JS. Tumblr é uma plataforma simples de blogs pessoais.",
+      type: "HTML",
+    },
+    {
+      id: 4,
+      image: "../../assets/Waves.png",
+      title: "Waves",
+      description:
+        "Este tema foi criado para ser vendido no Codester. Não foi usado nada além de HTML, CSS e JS. Tumblr é uma plataforma simples de blogs pessoais.",
+      type: "HTML",
+    },
+    {
+      id: 4,
+      image: "../../assets/Doravante.png",
+      title: "Doravante",
+      description:
+        "Este tema foi criado para ser vendido no Codester. Não foi usado nada além de HTML, CSS e JS. Tumblr é uma plataforma simples de blogs pessoais.",
+      type: "HTML",
+    },
+    {
+      id: 4,
+      image: "../../assets/Soli_Deo_Gloria.png",
+      title: "Soli Deo Gloria",
+      description:
+        "Este tema foi criado para ser vendido no Codester. Não foi usado nada além de HTML, CSS e JS. Tumblr é uma plataforma simples de blogs pessoais.",
+      type: "HTML",
+    },
+  ];
 
   const initBox = () => {
     $("aside").css({
@@ -26,35 +80,42 @@ export default function Modal(element, index) {
 
   initBox();
 
-  const insertSkills = () => {
-    const projects = [
-      {
-        id: 0,
-        image:
-          "https://images.unsplash.com/photo-1635765506301-536e9bb52dfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80",
-        title: "Project 1",
-        description: "lorem ipsum",
-      },
-      {
-        id: 1,
-        image:
-          "https://images.unsplash.com/photo-1635765506301-536e9bb52dfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80",
-        title: "Project 2",
-        description: "lorem ipsum",
-      },
-    ];
+  const insertProjects = () => {
+    const filters = document.querySelectorAll(".left ol li a");
+    filters.forEach((filter) => {
+      filter.addEventListener("click", function () {
+        $(".right").html("");
+        projects.forEach((project, index) => {
+          const projectType = project.type.toLowerCase();
+          const filterType = $(filter).attr("class").toLowerCase();
+
+          if (projectType === filterType) {
+            $(".right").append(
+              `<div class="project project-${index}"><div class="image-container"><img src="${project.image}" alt="${project.title}"/></div><div class="description"><h3>${project.title}</h3> ${project.description}</div></div>`
+            );
+          } else if (filterType === "all") {
+            $(".right").append(
+              `<div class="project project-${index}"><div class="image-container"><img src="${project.image}" alt="${project.title}"/></div><div class="description"><h3>${project.title}</h3> ${project.description}</div></div>`
+            );
+          }
+        });
+      });
+    });
+
+    $(".explorer").show();
 
     projects.forEach((project, index) => {
       $(".right").append(
-        `<div class="project project-${index}"><img src="${project.image}" alt="${project.title}" /><h3>${project.title}</h3><div class="description">${project.description}</div></div>`
+        `<div class="project project-${index}"><div class="image-container"><img src="${project.image}" alt="${project.title}"/></div><div class="description"><h3>${project.title}</h3> ${project.description}</div></div>`
       );
     });
+    // $(".project").css("height", $(".project").width());
   };
 
   const inputData = () => {
     switch (current) {
-      case "skills":
-        insertSkills();
+      case "projects":
+        insertProjects();
         break;
     }
   };
