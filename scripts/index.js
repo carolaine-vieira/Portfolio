4071efef5f59167f8b2b2ca3e696f21d78e37aaf
Carolaine Vieira <cavithemes@gmail.com>
2022-05-18 23:22:34 -0300

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".common-swiper", {
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   type: "fraction",
+  // },
+  navigation: {
+    nextEl: ".swiper_home_slide_pagination_next",
+    prevEl: ".swiper_home_slide_pagination_prev",
+  },
+});
