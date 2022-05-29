90eee11d137fcd4e4419d50b0aed4162724715e5
Carolaine Vieira <cavithemes@gmail.com>
2022-05-29 00:52:40 -0300

    feat: new version

--- scripts/home.js
+++ scripts/home.js
@@ -1,16 +1,134 @@
 var swiper = new Swiper(".common-swiper", {
-  navigation: {
-    nextEl: ".swiper_pagination_next",
-    prevEl: ".swiper_pagination_prev",
-  },
+  slidesPerView: 1,
+  spaceBetween: 20,
   loop: true,
-  initialSlide: 0,  
+  initialSlide: 0,
   breakpoints: {
     768: {
-      autoplay: {
-        delay: 4000,
-      },
-    }
-  }
+      slidesPerView: 2,
+    },
+  },
 });
 
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 355,
+      density: {
+        enable: true,
+        value_area: 789.1476416322727,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.48927153781200905,
+      random: false,
+      anim: {
+        enable: true,
+        speed: 0.2,
+        opacity_min: 0,
+        sync: false,
+      },
+    },
+    size: {
+      value: 2,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 2,
+        size_min: 0,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 0.2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 83.91608391608392,
+        size: 1,
+        duration: 3,
+        opacity: 1,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
+
+const handleSwiperSlide = () => {
+  const swipers = document.querySelectorAll(".swiper_home_slide");
+  if (swipers) {
+    swipers.forEach((swiper) => {
+      swiper.querySelector("img")?.addEventListener("click", (e) => {
+        swiper.classList.toggle("show-description");
+      });
+    });
+  }
+};
+handleSwiperSlide();
