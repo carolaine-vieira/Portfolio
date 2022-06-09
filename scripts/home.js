72c951e37b662e84770ecece5e19667f65cab6c4
Carolaine Vieira <cavithemes@gmail.com>
2022-06-09 09:43:30 -0300

    new version

--- scripts/home.js
+++ scripts/home.js
@@ -1,136 +1,30 @@
+import "./index.js";
+
 var sp = new Swiper(".common-swiper", {
   slidesPerView: 1,
-  spaceBetween: 20,
   loop: true,
-  initialSlide: 0,
+  spaceBetween: 30,
   breakpoints: {
     768: {
-      slidesPerView: 2,
+      slidesPerView: 3,
+      centeredSlides: true,
     },
   },
   autoplay: {
     delay: 3000,
     disableOnInteraction: true,
   },
-  
-});
-
-particlesJS("particles-js", {
-  particles: {
-    number: {
-      value: 355,
-      density: {
-        enable: true,
-        value_area: 789.1476416322727,
-      },
-    },
-    color: {
-      value: "#ffffff",
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000",
-      },
-      polygon: {
-        nb_sides: 5,
-      },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100,
-      },
-    },
-    opacity: {
-      value: 0.48927153781200905,
-      random: false,
-      anim: {
-        enable: true,
-        speed: 0.2,
-        opacity_min: 0,
-        sync: false,
-      },
-    },
-    size: {
-      value: 2,
-      random: true,
-      anim: {
-        enable: true,
-        speed: 2,
-        size_min: 0,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: false,
-      distance: 150,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 0.2,
-      direction: "none",
-      random: true,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200,
-      },
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "bubble",
-      },
-      onclick: {
-        enable: true,
-        mode: "push",
-      },
-      resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 83.91608391608392,
-        size: 1,
-        duration: 3,
-        opacity: 1,
-        speed: 3,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
-      },
-    },
-  },
-  retina_detect: true,
 });
 
 const handleSwiperSlide = () => {
   const swipers = document.querySelectorAll(".swiper_home_slide");
   if (swipers) {
     swipers.forEach((swiper) => {
-      swiper.querySelector("img")?.addEventListener("click", (e) => {
+      swiper.querySelector("img")?.addEventListener("click", () => {
         swiper.classList.toggle("show-description");
         sp.autoplay.stop();
       });
@@ -139,6 +33,6 @@ const handleSwiperSlide = () => {
 };
 handleSwiperSlide();
 
-if( window.innerWidth <= 768 ) {
+if (window.innerWidth <= 768) {
   sp.autoplay.stop();
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,26 @@
-const insertColors = () => {
-  const root = document.querySelector(':root');
-  const styles = JSON.parse(localStorage.getItem('styles'));
-  
-  if(styles) {
-    Object.entries(styles).forEach(([key, value]) => {
-      root.style.setProperty(key, value);
-      document.querySelector(`[data-color=${key}]`)?.setAttribute('value', value);
-    })
-  }
-}
-
 const mobileMenu = () => {
   const mobileMenuButton = document.querySelector('[data-menu="button"]');
-  const headerMenu = document.querySelector('.header__links').cloneNode(true);
+  const headerMenu = document.querySelector(".header__links").cloneNode(true);
 
-  const closeButton = document.createElement('div');
-  closeButton.classList.add('close-btn');
+  const closeButton = document.createElement("div");
+  closeButton.classList.add("close-btn");
   closeButton.innerHTML = '<span class="lnr lnr-cross"></span>';
 
-  const overlayContainer = document.createElement('div');
-  overlayContainer.classList.add('modal');
+  const overlayContainer = document.createElement("div");
+  overlayContainer.classList.add("modal");
 
-  mobileMenuButton.addEventListener('click', (e) => {
+  mobileMenuButton.addEventListener("click", (e) => {
     e.preventDefault();
-    
+
     overlayContainer.appendChild(closeButton);
     overlayContainer.appendChild(headerMenu);
-    overlayContainer.style.display = 'block';
+    overlayContainer.style.display = "block";
 
     document.body.append(overlayContainer);
-  })
+  });
 
-  closeButton.addEventListener('click', (e) => {
-    overlayContainer.style.display = 'none';
-  })
-}
-
-insertColors();
+  closeButton.addEventListener("click", (e) => {
+    overlayContainer.style.display = "none";
+  });
+};
 mobileMenu();
-
